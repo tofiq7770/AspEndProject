54d29c430f7f416cd12902c2d79d13bf538a1ffa
tofiq7770 <tofigtn@code.edu.az>
2024-06-18 14:34:41 +0400

    Update

--- AspEndProject/Controllers/ShopController.cs
+++ AspEndProject/Controllers/ShopController.cs
@@ -1,22 +1,39 @@
-﻿using AspEndProject.Services.Interface;
+﻿using AspEndProject.DAL;
+using AspEndProject.Models;
+using AspEndProject.Services.Interface;
+using AspEndProject.ViewModels;
+using AspEndProject.ViewModels.Category;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspEndProject.Controllers
 {
     public class ShopController : Controller
     {
+        private readonly AppDbContext _context;
         private readonly IProductService _productService;
         private readonly ICategoryService _categoryService;
 
-        public ShopController(IProductService productService, ICategoryService categoryService)
+        public ShopController(IProductService productService,
+                              ICategoryService categoryService,
+                              AppDbContext context)
         {
+            _context = context;
             _productService = productService;
             _categoryService = categoryService;
         }
 
+
         public async Task<IActionResult> Index()
         {
-            var datas = await _productService.GetAllAsync();
+            List<Product> products = await _productService.GetAllAsyncAscending();
+            Product product = _context.Products.FirstOrDefault();
+            List<CategoryVM> categories = await _categoryService.GetAllCategoriesAsc();
+            ShopVM datas = new()
+            {
+                Products = products,
+                Product = product,
+                Categories = categories
+            };
 
             return View(datas);
         }
---
+++ AspEndProject/ViewModels/ShopVM.cs
@@ -0,0 +1,14 @@
+﻿using AspEndProject.Models;
+using AspEndProject.ViewModels.Category;
+
+namespace AspEndProject.ViewModels
+{
+    public class ShopVM
+    {
+
+        public List<Product> Products { get; set; }
+        public Product Product { get; set; }
+        public List<CategoryVM> Categories { get; set; }
+
+    }
+}
